ca27ceec0339f1af783770ca6bd96088b961d79f
j4chaves <jacobjc3@gmail.com>
2019-11-17 00:33:33 -0500

    Update PayStubReader.java
    Refactored logic into separate functions.

--- src/main/java/com/jay/paystub2budget/paystub/PayStubReader.java
+++ src/main/java/com/jay/paystub2budget/paystub/PayStubReader.java
@@ -29,67 +29,16 @@ public class PayStubReader {
 	public static final String CHECK_DATE = "Check Date:";
 	
 	private Map<String, StubField> extractedFields = new HashMap<String, StubField>();
-	private Date date;
+	private Date date;		// TODO should be changed to paystubDate
+	private File file;
 	
 	public PayStubReader (File file) {
-		
-		PDDocument doc;
-		try {
-			PDFTextStripper stripper = new PDFTextStripper();
-			stripper.setLineSeparator("\n");
-			stripper.setStartPage(1);
-			stripper.setEndPage(1);
-			doc = PDDocument.load(file);
-			String contents = stripper.getText(doc);
-			doc.close();
-			
-			
-			List<String> contentsList = Arrays.asList(contents.split("\n"));
-			ArrayList<String> splitContents = new ArrayList<String>(contentsList);
-			
-			for(String line : splitContents) {
-				if (line.contains(TOTAL_TAXES_WITHHELD)) {
-					String[] splitStr = splitField(line, TOTAL_TAXES_WITHHELD);
-					
-					double current = Double.valueOf(splitStr[0]);
-					double yearToDate = Double.valueOf(splitStr[1]);
-					
-					String fieldName = line.substring(0, TOTAL_TAXES_WITHHELD.length()).trim();
-					extractedFields.put(fieldName, new StubField(fieldName, current, yearToDate));
-				} else if (line.contains(NET_PAY)) {
-					String[] splitStr = splitField(line, NET_PAY);
-					
-					double current = Double.valueOf(splitStr[0]);
-					double yearToDate = Double.valueOf(splitStr[1]);
-					
-					String fieldName = line.substring(0, NET_PAY.length()).trim();
-					extractedFields.put(fieldName, new StubField(fieldName, current, yearToDate));
-				}	else if (line.contains(TOTAL_PRETAX_DEDUCTIONS)) {
-					String[] splitStr = splitField(line, TOTAL_PRETAX_DEDUCTIONS);
-					
-					double current = Double.valueOf(splitStr[0]);
-					double yearToDate = Double.valueOf(splitStr[1]);
-					
-					String fieldName = line.substring(0, TOTAL_PRETAX_DEDUCTIONS.length()).trim();
-					extractedFields.put(fieldName, new StubField(fieldName, current, yearToDate));
-				}  else if (line.contains(CHECK_DATE)) {
-					String subString = line.substring(CHECK_DATE.length()).trim();
-					String dateString = subString.split(" ")[0];
-					DateFormat format = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
-					try {
-						date = format.parse(dateString);
-					} catch (ParseException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-			extractedFields.forEach((key,value) -> System.out.println(value.toString()));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		this.file = file;
 	}
 	
 	public PayStub createPayStubObject() {
+		parseData();
+		
 		PayStub stub = new PayStub();
 		stub.setDate(date);
 		stub.setExtractedFields(extractedFields);
@@ -99,6 +48,78 @@ public class PayStubReader {
 		Months month = Months.valueOf(monthString);
 		stub.setMonth(month);
 		return stub;
+	}
+	
+	private void parseData() {
+
+		String contents = readFile();
+		
+		List<String> contentsList = Arrays.asList(contents.split("\n"));
+		ArrayList<String> splitContents = new ArrayList<String>(contentsList);
+
+		for(String line : splitContents) {
+			if (line.contains(TOTAL_TAXES_WITHHELD)) {
+				String[] splitStr = splitField(line, TOTAL_TAXES_WITHHELD);
+
+				double current = Double.valueOf(splitStr[0]);
+				double yearToDate = Double.valueOf(splitStr[1]);
+
+				String fieldName = line.substring(0, TOTAL_TAXES_WITHHELD.length()).trim();
+				extractedFields.put(fieldName, new StubField(fieldName, current, yearToDate));
+			} else if (line.contains(NET_PAY)) {
+				String[] splitStr = splitField(line, NET_PAY);
+
+				double current = Double.valueOf(splitStr[0]);
+				double yearToDate = Double.valueOf(splitStr[1]);
+
+				String fieldName = line.substring(0, NET_PAY.length()).trim();
+				extractedFields.put(fieldName, new StubField(fieldName, current, yearToDate));
+			}	else if (line.contains(TOTAL_PRETAX_DEDUCTIONS)) {
+				String[] splitStr = splitField(line, TOTAL_PRETAX_DEDUCTIONS);
+
+				double current = Double.valueOf(splitStr[0]);
+				double yearToDate = Double.valueOf(splitStr[1]);
+
+				String fieldName = line.substring(0, TOTAL_PRETAX_DEDUCTIONS.length()).trim();
+				extractedFields.put(fieldName, new StubField(fieldName, current, yearToDate));
+			}  else if (line.contains(CHECK_DATE)) {
+				String subString = line.substring(CHECK_DATE.length()).trim();
+				String dateString = subString.split(" ")[0];
+				DateFormat format = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+				try {
+					date = format.parse(dateString);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		extractedFields.forEach((key,value) -> System.out.println(value.toString()));
+	}
+
+	public String readFile() {
+		
+		String contents = "";
+
+		if (this.file != null || !this.file.exists()) {
+			//contents = "";	// TODO Handle better
+		}
+		
+		PDDocument doc;
+		
+		try {
+			PDFTextStripper stripper = new PDFTextStripper();
+			stripper.setLineSeparator("\n");
+			stripper.setStartPage(1);
+			stripper.setEndPage(1);
+			doc = PDDocument.load(file);
+			contents = stripper.getText(doc);
+			doc.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return contents;
 	}
 	
 	private String[] splitField(String line, String field) {		
